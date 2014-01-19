f3ec42025c53011f3d14c81abd2d239ebb5ebbf1
Marcel Farres <marcel.farres@gmail.com>
2014-01-20 00:54:49 +0100

    fill input
    we need to migrate old funcionalities form the main

--- lab1/input.h
+++ lab1/input.h
@@ -4,7 +4,6 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/type_ptr.hpp"
-#include "Light.h"
 #include <string>
 #include <cstdlib>
 #include <iostream>
@@ -14,7 +13,29 @@
 class input
 {
 public:
+	// Var
+	int height;			// Screen height
+	int width;			// Screen width 
+	glm::mat4 Projection;
+	glm::mat4 View;
+	glm::vec3 Camera;
+
+	// Fun
 	input();
 	~input();
+	void reset();
+	void updateProjection();
+	void updateView();
+	void onReshape(int width, int height);
+	void MyKeyboardFunc(unsigned char Key, int x, int y);
+	void SetZoom(float in_zoom);
+	void SetDolly(float in_dolly);
+
+private:
+	float sf_trlz[4];
+	int max_col;
+	float zoom;
+	float dolly;
+	glm::vec3 direction;
 };
 
