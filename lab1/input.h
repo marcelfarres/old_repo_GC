75496f5ea130606c75e3ba9bf5b666bf1752b70f
Marcel Farres <marcel.farres@gmail.com>
2014-01-20 00:37:41 +0100

    Add new class input
    Camera matrix transform + inputs form keyboard

---
+++ lab1/input.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "Light.h"
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class input
+{
+public:
+	input();
+	~input();
+};
+
