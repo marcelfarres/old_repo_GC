d12178ba89d08736a6aa6ce8bea30fac6afab0d7
Marcel Farres <marcel.farres@gmail.com>
2014-01-21 17:09:04 +0100

    bugs fixed

--- lab1/input.h
+++ lab1/input.h
@@ -1,9 +1,7 @@
 #pragma once
-#include "GL/glew.h"
-#include "GL/freeglut.h"
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
 #include <string>
 #include <cstdlib>
 #include <iostream>
