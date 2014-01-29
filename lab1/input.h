cd70fefb4c32a20a135c0782cbaeaadfbc334768
Marcel Farres <marcel.farres@gmail.com>
2014-01-29 18:13:17 +0100

    re-structuration

--- lab1/input.h
+++ lab1/input.h
@@ -21,10 +21,6 @@ public:
 	void onReshape(int width, int height);
 	void MyKeyboardFunc(unsigned char Key, int x, int y);
 	void MyKeyboardFunc(int Key, int x, int y);
-	void SetZoom(float in_zoom);
-	void SetDolly(float in_dolly);
-	void MoveObj(float in_x, float in_y, float in_z);
-	void RotObj(float in_x, float in_y, float in_z, float dir);
 
 private:
 	float sf_trlz[3];
@@ -38,5 +34,10 @@ private:
 	glm::mat4 model;
 	float dolly;
 	float zoom;
+
+	void SetZoom(float in_zoom);
+	void SetDolly(float in_dolly);
+	void MoveObj(float in_x, float in_y, float in_z);
+	void RotObj(float in_x, float in_y, float in_z, float dir);
 };
 #endif
